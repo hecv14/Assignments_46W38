1105caad0d5c37a633778034865b3eb72346dff5
hecv14 <hectormvl@gmail.com>
2025-09-23 22:53:00 +0200

    translation to deal with punctuation added

--- module04/task_two.py
+++ module04/task_two.py
@@ -1,20 +1,32 @@
+# %% imports
+import string
+
 # %% custom functions
 def count_words(file):
 
     def get_words_in_txt(file):
         """Open a text file & return a list of strings."""
         try:
-            with open(file) as text_read:
-                words = text_read.read().split()
+            with open(file) as f:
+                words = f.read().translate(str.maketrans("", "", string.punctuation)).split()
             return words
         except FileNotFoundError:
             print(f'{file} not found. Check argument.')
-
+    
     words_count = len(get_words_in_txt(file))
     return words_count
 
+# translate explanation:
+# string.punctuation contains all common punctuation characters:
+# !"#$%&'()*+,-./:;<=>?@[\]^_{|}~
+# str.maketrans("", "", string.punctuation) creates a translation table that
+# maps all punctuation to None.
+# .translate(...) removes all characters from the text that are in
+# string.punctuation.
 
 # %% application
 number_words = count_words("The_Zen_of_Python.txt")
 print(number_words)
 # TODO: do not count punctuation
+
+# %%
